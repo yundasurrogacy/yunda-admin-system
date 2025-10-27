8afea81db5f304075f8957729b6cb3efab44bf27
1903812532 <1903812532@qq.com>
2025-10-27 08:12:53 +0800

    还原博客富文本

--- src/app/admin/documents/loading.tsx
+++ src/app/admin/documents/loading.tsx
@@ -1,9 +1,3 @@
-'use client';
-
 export default function Loading() {
-  return (
-    <div className="flex items-center justify-center h-screen">
-      <div className="text-lg text-sage-700">Loading...</div>
-    </div>
-  );
+  return null
 }
